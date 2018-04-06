2c2803ea97074d191882cf24362196c760066b85
Manuel Avello <manuelavello@Manuels-MacBook-Pro.local>
2018-04-06 18:44:57 +0200

    maketado version escritorio

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // default value for title local
-app.locals.title = 'TOKEN LIKE';
+app.locals.title = 'PHILIKETROPY';
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
